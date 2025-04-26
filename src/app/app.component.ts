f5d59f67fd8a5dbd43c2773a28a85a78b518720b
Sinju-Varghese <sinjuvarghese58@gmail.com>
2025-04-27 01:15:51 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,49 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'], 
 })
 export class AppComponent {
   title = 'opencart';
-  message="hello world";
-  numbers = [1, 3 , 5 ,6];
-  rows = 5;
-  dateTime = '';
-  color = 'red';
-  classNamesString = 'text-red text-center';
-  classNamesList = ['text-red', 'text-center'];
 
-  count = 0;
-    handleClick(){
-      this.count++;
-    }
-    
-  constructor(){
-    setInterval(() => {
-      this.color === 'red' ? this.color = 'blue' : this.color = 'red';
-      const date = new Date();
-      this.dateTime = date.toLocaleString()
-    },1000)
+  backgroundColor = 'white';
+  height = 200;
+  width = 150;
+  minHeight = 0;
+  maxHeight = 400;
+  minWidth = 0;
+  maxWidth = 300;
+  rotation = 0;
 
-     
+  handleClickRed() {
+    this.backgroundColor = 'red';
   }
 
+  handleClickGreen() {
+    this.backgroundColor = 'green';
+  }
 
+  handleClickBlue() {
+    this.backgroundColor = 'blue';
+  }
+
+  handleHeight(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.height = Number(target.value);
+  }
+
+  handleWidth(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.width = Number(target.value);
+  }
+
+  handleRotationPlus() {
+    this.rotation += 6;
+  }
+
+  handleRotationMinus() {
+    this.rotation -= 6;
+  }
 }
