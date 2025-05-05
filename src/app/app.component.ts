aa54e488854a6931b9f612fda94fe36e9d9763c1
Sinju-Varghese <sinjuvarghese58@gmail.com>
2025-05-05 14:20:49 +0530

    completed form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,42 +9,75 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'opencart';
 
-  backgroundColor = 'white';
-  height = 200;
-  width = 150;
-  minHeight = 0;
-  maxHeight = 400;
-  minWidth = 0;
-  maxWidth = 300;
-  rotation = 0;
+  // isSucccess= true;
 
-  handleClickRed() {
-    this.backgroundColor = 'red';
-  }
+  // fullName = 'Sinju varghese';
+  
+ 
+  // backgroundColor = 'white';
+  // height = 200;
+  // width = 150;
+  // minHeight = 0;
+  // maxHeight = 400;
+  // minWidth = 0;
+  // maxWidth = 300;
+  // rotation = 0;
 
-  handleClickGreen() {
-    this.backgroundColor = 'green';
-  }
+  // handleClickRed() {
+  //   this.backgroundColor = 'red';
+  // }
 
-  handleClickBlue() {
-    this.backgroundColor = 'blue';
-  }
+  // handleClickGreen() {
+  //   this.backgroundColor = 'green';
+  // }
 
-  handleHeight(event: Event) {
-    const target = event.target as HTMLInputElement;
-    this.height = Number(target.value);
-  }
+  // handleClickBlue() {
+  //   this.backgroundColor = 'blue';
+  // }
 
-  handleWidth(event: Event) {
-    const target = event.target as HTMLInputElement;
-    this.width = Number(target.value);
-  }
+  // handleHeight(event: Event) {
+  //   const target = event.target as HTMLInputElement;
+  //   this.height = Number(target.value);
+  // }
 
-  handleRotationPlus() {
-    this.rotation += 6;
-  }
+  // handleWidth(event: Event) {
+  //   const target = event.target as HTMLInputElement;
+  //   this.width = Number(target.value);
+  // }
 
-  handleRotationMinus() {
-    this.rotation -= 6;
+  // handleRotationPlus() {
+  //   this.rotation += 6;
+  // }
+
+  // handleRotationMinus() {
+  //   this.rotation -= 6;
+  // }
+
+
+  // form
+
+  student = {
+    firstName: '',
+    lastName: '',
+    class: '',
+    age: null,
+    mark1: null,
+    mark2: null,
+    mark3: null,
+    favColor: '',
+    gender: ''
+  };
+
+  classes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  sentence: string = '';
+
+  generateSentence() {
+    const { firstName, lastName, class: classNum, age, gender, favColor, mark1, mark2, mark3 } = this.student;
+    const totalMarks = (mark1 || 0) + (mark2 || 0) + (mark3 || 0); // Handles null values
+    const pronoun = gender === 'Female' ? 'She' : 'He';
+    const possessivePronoun = gender === 'Female' ? 'Her' : 'His';
+  
+    this.sentence = `${firstName} ${lastName} of class ${classNum} is ${age} years old. ${pronoun} scored ${totalMarks} marks in the exam. ${possessivePronoun} favorite color is ${favColor}.`;
   }
+  
 }
